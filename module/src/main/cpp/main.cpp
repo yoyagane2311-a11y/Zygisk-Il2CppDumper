21ad4a2b7fe18ac8835b37ea1f3f7c4f3d1a74f9
yoyagane2311-a11y <yoyagane2311@gmail.com>
2026-03-28 18:44:42 -0300

    Update main.cpp

--- module/src/main/cpp/main.cpp
+++ module/src/main/cpp/main.cpp
@@ -13,7 +13,6 @@
 
 using zygisk::Api;
 using zygisk::AppSpecializeArgs;
-using zygisk::ServerSpecializeArgs;
 
 class MyModule : public zygisk::ModuleBase {
 public:
@@ -23,58 +22,39 @@ public:
     }
 
     void preAppSpecialize(AppSpecializeArgs *args) override {
+        if (!args->nice_name) return;
         auto package_name = env->GetStringUTFChars(args->nice_name, nullptr);
         auto app_data_dir = env->GetStringUTFChars(args->app_data_dir, nullptr);
-        preSpecialize(package_name, app_data_dir);
+        
+        // Verificamos si es Standoff 2
+        if (package_name && strcmp(package_name, GamePackageName) == 0) {
+            LOGI("¡Juego detectado!: %s", package_name);
+            enable_hack = true;
+            game_data_dir = new char[strlen(app_data_dir) + 1];
+            strcpy(game_data_dir, app_data_dir);
+        }
+        
         env->ReleaseStringUTFChars(args->nice_name, package_name);
         env->ReleaseStringUTFChars(args->app_data_dir, app_data_dir);
     }
 
     void postAppSpecialize(const AppSpecializeArgs *) override {
         if (enable_hack) {
+            // Iniciamos el hack/dumper en un hilo separado
+            // IMPORTANTE: Asegurate que en hack.cpp la búsqueda apunte a "libunity.so"
             std::thread hack_thread(hack_prepare, game_data_dir, data, length);
             hack_thread.detach();
+            LOGI("Hilo de inyección lanzado para Standoff 2");
         }
     }
 
 private:
     Api *api;
     JNIEnv *env;
-    bool enable_hack;
-    char *game_data_dir;
-    void *data;
-    size_t length;
-
-    void preSpecialize(const char *package_name, const char *app_data_dir) {
-        if (strcmp(package_name, GamePackageName) == 0) {
-            LOGI("detect game: %s", package_name);
-            enable_hack = true;
-            game_data_dir = new char[strlen(app_data_dir) + 1];
-            strcpy(game_data_dir, app_data_dir);
-
-#if defined(__i386__)
-            auto path = "zygisk/armeabi-v7a.so";
-#endif
-#if defined(__x86_64__)
-            auto path = "zygisk/arm64-v8a.so";
-#endif
-#if defined(__i386__) || defined(__x86_64__)
-            int dirfd = api->getModuleDir();
-            int fd = openat(dirfd, path, O_RDONLY);
-            if (fd != -1) {
-                struct stat sb{};
-                fstat(fd, &sb);
-                length = sb.st_size;
-                data = mmap(nullptr, length, PROT_READ, MAP_PRIVATE, fd, 0);
-                close(fd);
-            } else {
-                LOGW("Unable to open arm file");
-            }
-#endif
-        } else {
-            api->setOption(zygisk::Option::DLCLOSE_MODULE_LIBRARY);
-        }
-    }
+    bool enable_hack = false;
+    char *game_data_dir = nullptr;
+    void *data = nullptr;
+    size_t length = 0;
 };
 
 REGISTER_ZYGISK_MODULE(MyModule)
